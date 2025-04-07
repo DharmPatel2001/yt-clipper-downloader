e3fbb149589dba34262583e36ec86c601300dcae
keval7298 <vithanikeval7298@gmail.com>
2025-04-07 12:45:41 +0530

    urls.py file fix

--- youtube_video_clipper/urls.py
+++ youtube_video_clipper/urls.py
@@ -1,10 +1,8 @@
 # youtube_video_clipper/urls.py
 from django.contrib import admin
 from django.urls import path, include
-from django.shortcuts import redirect
 
 urlpatterns = [
-    path('', lambda request: redirect('admin:index')),
+    path('', include('video_clipper.urls')),
     path('admin/', admin.site.urls),
-    path('clip/', include('video_clipper.urls')),
 ]
