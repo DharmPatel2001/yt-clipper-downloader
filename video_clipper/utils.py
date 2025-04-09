0a27672e5bba87ae5fd8135b4d9b6320ca1170e9
keval7298 <vithanikeval7298@gmail.com>
2025-04-09 12:37:26 +0530

    utils file change

--- video_clipper/utils.py
+++ video_clipper/utils.py
@@ -79,7 +79,6 @@ def find_song_data(youtube_url, file_path, start_time, end_time, video_download,
         }],
         'noplaylist': True,
         'keepvideo': True,
-        'cookiefile': 'cookies.txt',
         'http_headers': {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
